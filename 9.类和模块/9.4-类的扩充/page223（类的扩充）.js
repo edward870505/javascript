a4295f97b1f7db5b5265e62057929737e046a21e
edward@PC <edward.huang@foxmail.com>
2019-03-15 06:22:26 +0800

    2019-3-15

--- 9.类和模块/9.4-类的扩充/page223（类的扩充）.js
+++ 9.类和模块/9.4-类的扩充/page223（类的扩充）.js
@@ -12,4 +12,16 @@ n.times(function(n){console.log(n+"hello");})
 
 //如果不存在ES5的String.trim()方法的话，就定义它
 //这个方法用以去除字符串开头和结尾的空格
+String.prototype.trim = String.prototype.trim || function(){
+    if(!this) return this;//空字符不做处理
+    return this.replace(/^\s+ |\s+$/g,"");//使用正则表达式进行空格替换
+}
+
+//返回函数的名字，如果它有（非标准的）name属性，则直接使用name属性
+//否则，将函数转换为字符串然后从中提取名字
+//如果是没有名字的函数，则返回一个空字符串
+Function.prototype.getName = function(){
+    return this.name || this.toString.match(/function\s*([^()*]\(/)[1];
+}
+
 
